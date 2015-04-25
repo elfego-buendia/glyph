93fc9c86fe93098a0d9587558ab48b4b5cf3cc13
Jose Martinez <elfego.buendia.x@gmail.com>
2015-04-24 21:24:52 -0500

    Fixed floating input issue removed code for features that are not ready

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@ var selectedGlyph = null;
 $(function () {
     // set the index of the select element to match the default block generated
     var index  = $('#default-block').prop('index');
-    var gBlock = $('#glyph-block');
-    
+    var gBlock = $('#gb-select');
+
     gBlock.prop('selectedIndex', index);
-    
+
     // default block to display
     generateAndBind('2600', '26FF');
-    
+
     // generate based on selection
     gBlock.change(function() {
         var option = this.options[this.selectedIndex];
-        
+
         var range;
         if(option.id == 'customb') {
             range = ['2630', '2637'];
@@ -24,7 +24,7 @@ $(function () {
 
         generateAndBind(range[0], range[1]);
     });
-    
+
     $('#user-glyphs').click(function (e) {
         this.setSelectionRange(0,999);
     });
@@ -33,18 +33,19 @@ $(function () {
 function generateAndBind(from, to) {
     var gen1 = makeGlyphs(from, to);
     $('.generated').html(gen1);
-    
+
     var glyph      = $('.glyph');
     var userGlyphs = $('#user-glyphs');
-    
+
     glyph.hammer().bind('tap', function(e) {
         // triggers reflow which allows CSS animation to retrigger
         $(this).removeClass('selected');
         this.offsetWidth = this.offsetWidth;
         $(this).addClass('selected');
-        
+
         userGlyphs.val(userGlyphs.val() + $(this).text());
     });
+    /*
     glyph.hammer().bind('press', function (e) {
         if (selectedGlyph === null) {
             selectedGlyph = this;
@@ -56,7 +57,8 @@ function generateAndBind(from, to) {
         }
     });
     glyph.hammer().bind('swipe', function (e) {
-       $(this).removeClass('magnified'); 
+       $(this).removeClass('magnified');
        selectedGlyph = null;
     });
+    */
 }
