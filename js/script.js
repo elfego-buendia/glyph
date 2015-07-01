4c05eb5a297f92a55c81fb23ed7a635c6b42c02a
Jose Martinez <elfego.buendia.x@gmail.com>
2015-06-30 19:44:25 -0500

    Refactored JS

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
 /*eslint-env browser*/
 /*eslint no-multi-spaces:0, no-use-before-define:0, no-unused-vars:0, no-alert:0 */
-/*global $, e*/
+/*global $, e, makeGlyphs*/
 
 $(function () {
-    'use strict';
-    // set the index of the select element to match the default block generated
-    var index  = $('#default-block').prop('index');
-    var gBlock = $('#select-glyph-block');
+    /*
+     * Functions
+     */
 
-    gBlock.prop('selectedIndex', index);
+     function generateAndBind(from, to) {
+         var generatedGlyphs = makeGlyphs(from, to);
+         $('.generated').html(generatedGlyphs);
 
-    // default block to display
-    generateAndBind('2600', '26FF');
+         var glyph      = $('.glyph');
+         var userGlyphs = $('#user-glyphs');
 
-    // stop the window from scrolling after events from select and last custom input element
-    $(document).on('blur', '#select-glyph-block,#input-to', function() {
-        window.scrollTo(0, 0);
-    });
+         glyph.hammer().bind('tap', function(e) {
+             // triggers reflow which allows CSS animation to retrigger
+             $(this).removeClass('selected');
+             this.offsetWidth = this.offsetWidth;
+             $(this).addClass('selected');
 
-    /* move fixed textarea and make backspace appear, for better view while in landscape */
+             userGlyphs.val(userGlyphs.val() + $(this).text());
+         });
+     }
+
+
+    /*
+     * Set up event handlers
+     */
+
+    // move fixed textarea and make backspace appear, for better view while in landscape
     window.onscroll = function() {
       if(window.pageYOffset >= 60) {
         $('#user-glyphs').addClass('user-glyphs-min');
@@ -28,6 +39,51 @@ $(function () {
         $('#backspace').removeClass('show-backspace');
       }
     };
+
+    // select all when in focus, for easy copying
+    $('#user-glyphs').click(function () {
+        this.setSelectionRange(0, 999);
+    });
+
+    // stop the window from scrolling after events from select and last custom input element. mobile only
+    $(this).on('blur', '#select-glyph-block,#input-to', function() {
+        window.scrollTo(0, 0);
+    });
+
+    /* go click, process custom range, error if invalid range */
+    $('#input-go').click(function (e) {
+      var fRange = $('#input-from').val();
+      var tRange = $('#input-to').val();
+
+      if(parseInt(fRange, 16) >= parseInt(tRange, 16)) {
+        alert('Please enter a valid range');
+      } else {
+        generateAndBind(fRange, tRange);
+      }
+    });
+
+    // backspace click
+    $('#backspace').click(function() {
+      var inString = $('#user-glyphs').val();
+      $('#user-glyphs').val(inString.substring(0, inString.length - 1));
+    });
+
+    // e
+    $('#email').click(function () {
+        var first = 'elfego', second = 'buendia', third = 'x', end = '@gmail.com', subject = '?Subject=Glyph';
+        $(this).attr('href', 'mailto:' + first + '.' + second + '.' + third + end + subject);
+    });
+
+
+    /*
+     * Initial setup
+     */
+
+    // set the index of the select element to match the default block generated
+    var index  = $('#default-block').prop('index');
+    var gBlock = $('#select-glyph-block');
+
+    gBlock.prop('selectedIndex', index);
 
     // generate based on selection
     gBlock.change(function(e) {
@@ -43,50 +99,6 @@ $(function () {
         }
     });
 
-    /* go click, process custom range, error if invalid range */
-    $('#input-go').click(function (e) {
-      var fRange = $('#input-from').val();
-      var tRange = $('#input-to').val();
-
-      if(parseInt(fRange, 16) >= parseInt(tRange, 16)) {
-        alert('Please enter a valid range');
-      } else {
-        generateAndBind(fRange, tRange);
-      }
-    });
-
-    /* select all when in focus, for easy copying */
-    $('#user-glyphs').click(function () {
-        this.setSelectionRange(0, 999);
-    });
-
-    /* backspace click */
-    $('#backspace').click(function() {
-      var inString = $('#user-glyphs').val();
-      $('#user-glyphs').val(inString.substring(0, inString.length - 1));
-    });
-
-    /* e */
-    $('#email').click(function () {
-        var first = 'elfego', second = 'buendia', third = 'x', end = '@gmail.com', subject = '?Subject=Glyph';
-        $(this).attr('href', 'mailto:' + first + '.' + second + '.' + third + end + subject);
-    });
+    // default block to display
+    generateAndBind('2600', '26FF');
 });
-
-function generateAndBind(from, to) {
-    
-    var gen1 = makeGlyphs(from, to);
-    $('.generated').html(gen1);
-
-    var glyph      = $('.glyph');
-    var userGlyphs = $('#user-glyphs');
-
-    glyph.hammer().bind('tap', function(e) {
-        // triggers reflow which allows CSS animation to retrigger
-        $(this).removeClass('selected');
-        this.offsetWidth = this.offsetWidth;
-        $(this).addClass('selected');
-
-        userGlyphs.val(userGlyphs.val() + $(this).text());
-    });
-}
