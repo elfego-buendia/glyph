7a4651028f8f506dc144bb8ebbc46871b164a3c6
Jose Martinez <elfego.buendia.x@gmail.com>
2015-06-30 19:17:34 -0500

    Refactored CSS and some JS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-// selected global
-var selectedGlyph = null;
+/*eslint-env browser*/
+/*eslint no-multi-spaces:0, no-use-before-define:0, no-unused-vars:0, no-alert:0 */
+/*global $, e*/
 
 $(function () {
+    'use strict';
     // set the index of the select element to match the default block generated
     var index  = $('#default-block').prop('index');
-    var gBlock = $('#gb-select');
+    var gBlock = $('#select-glyph-block');
 
     gBlock.prop('selectedIndex', index);
 
@@ -12,39 +14,39 @@ $(function () {
     generateAndBind('2600', '26FF');
 
     // stop the window from scrolling after events from select and last custom input element
-    $(document).on('blur', '#gb-select,#to', function() {
-        window.scrollTo(0,0);
+    $(document).on('blur', '#select-glyph-block,#input-to', function() {
+        window.scrollTo(0, 0);
     });
 
     /* move fixed textarea and make backspace appear, for better view while in landscape */
     window.onscroll = function() {
       if(window.pageYOffset >= 60) {
-        $('#user-glyphs').addClass('ug-min');
-        $('#backspace').addClass('bs-show');
-      } else if (window.pageYOffset <= 30)  {
-        $('#user-glyphs').removeClass('ug-min');
-        $('#backspace').removeClass('bs-show');
+        $('#user-glyphs').addClass('user-glyphs-min');
+        $('#backspace').addClass('show-backspace');
+      } else if (window.pageYOffset <= 30) {
+        $('#user-glyphs').removeClass('user-glyphs-min');
+        $('#backspace').removeClass('show-backspace');
       }
-    }
+    };
 
     // generate based on selection
     gBlock.change(function(e) {
         var option = this.options[this.selectedIndex];
 
         var range;
-        if(option.id == 'customb') {
-            $('.cb-row').addClass('cb-show');
+        if(option.id === 'custom-block') {
+            $('.row-custom-block').addClass('show-custom-block');
         } else {
-            $('.cb-row').removeClass('cb-show');
+            $('.row-custom-block').removeClass('show-custom-block');
             range = option.value.split('-');
             generateAndBind(range[0], range[1]);
         }
     });
 
     /* go click, process custom range, error if invalid range */
-    $('#go').click(function (e) {
-      var fRange = $('#from').val();
-      var tRange = $('#to').val();
+    $('#input-go').click(function (e) {
+      var fRange = $('#input-from').val();
+      var tRange = $('#input-to').val();
 
       if(parseInt(fRange, 16) >= parseInt(tRange, 16)) {
         alert('Please enter a valid range');
@@ -55,23 +57,24 @@ $(function () {
 
     /* select all when in focus, for easy copying */
     $('#user-glyphs').click(function () {
-        this.setSelectionRange(0,999);
+        this.setSelectionRange(0, 999);
     });
 
     /* backspace click */
     $('#backspace').click(function() {
       var inString = $('#user-glyphs').val();
-      $('#user-glyphs').val(inString.substring(0, inString.length - 1))
+      $('#user-glyphs').val(inString.substring(0, inString.length - 1));
     });
 
     /* e */
-    $('#em').click(function () {
+    $('#email').click(function () {
         var first = 'elfego', second = 'buendia', third = 'x', end = '@gmail.com', subject = '?Subject=Glyph';
         $(this).attr('href', 'mailto:' + first + '.' + second + '.' + third + end + subject);
     });
 });
 
 function generateAndBind(from, to) {
+    
     var gen1 = makeGlyphs(from, to);
     $('.generated').html(gen1);
 
